53a09b9af4b75c99062e5ed656e930dd6e2733bf
Hezekiah M. Carty <hez@0ok.org>
2011-07-10 21:29:55 -0400

    The base for the new oasis-based build system
    Some customization to myocamlbuild.ml is required in order to properly use
    h4cc to compile the C stubs.  This also includes a commented out section
    to support camlidl.  It will be removed in a future commit.

---
+++ myocamlbuild.ml
@@ -0,0 +1,69 @@
+(* OASIS_START *)
+(* OASIS_STOP *)
+
+open Ocamlbuild_plugin;;
+open Command;;
+
+(* The HDF4 compiler script (a wrapper around gcc, generally) *)
+let h4cc =
+  BaseEnvLight.var_get "h4cc" (
+    BaseEnvLight.load
+      ~allow_empty:true ~filename:MyOCamlbuildBase.env_filename ()
+  )
+;;
+
+let run_and_read = Ocamlbuild_pack.My_unix.run_and_read;;
+let blank_sep_strings = Ocamlbuild_pack.Lexers.blank_sep_strings;;
+
+(* Link flags appropriate for programs using HDF4, taken from h4cc *)
+let hdf4_linkflags cclib =
+  let tag = if cclib then A "-cclib" else N in
+  let all_elements =
+    blank_sep_strings (
+      Lexing.from_string (
+        run_and_read (h4cc ^ " -show")
+      )
+    )
+  in
+  let is_linker_flag s = s.[0] = '-' && (s.[1] = 'l' || s.[1] = 'L') in
+  let flags = List.filter is_linker_flag all_elements in
+  S (List.map (fun flag -> S [tag; A flag]) flags)
+;;
+
+let hdf4_dispatch = function
+  | After_options ->
+      (* Use h4cc for C compilation *)
+      Options.ocamlc := S [!Options.ocamlc; A "-cc"; A h4cc];
+  | After_rules ->
+      (* Keeping the camlidl rule around temporarily in case it's useful later
+      (* Handle *.idl files properly... I think *)
+      rule "camlidl processing"
+        ~prods:["%.mli"; "%.ml"; "%_stubs.c"]
+        ~deps:["%.idl"]
+        begin fun env _build ->
+          let idl = env "%.idl" in
+          let tags = tags_of_pathname idl++"compile"++"camlidl" in
+          let cmd = Cmd(S[camlidl; T tags; P idl]) in
+          Seq [cmd]
+        end;
+      *)
+
+      (* PIC for 64bit systems *)
+      flag ["c"; "compile"] (S [A "-ccopt"; A "-fPIC"]);
+
+      (* Include HDF4 linking options for ocamlmklib *)
+      flag ["ocamlmklib"; "c"] (hdf4_linkflags false);
+
+      (* Use the proper extras when compiling the OCaml library *)
+      flag ["ocaml"; "link"; "library"; "byte"] (hdf4_linkflags true);
+      flag ["ocaml"; "link"; "library"; "native"] (hdf4_linkflags true);
+  | _ -> ()
+;;
+
+dispatch (
+  MyOCamlbuildBase.dispatch_combine [
+    dispatch_default;
+    hdf4_dispatch;
+  ]
+)
+;;
